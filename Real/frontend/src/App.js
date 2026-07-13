7b2a1a3af1b654ef4e366638a08ab734ba8dc2f6
George Michaelides <george_michaelides@asl.org>
2026-07-13 17:44:11 +0100

    Fixed title

--- Real/frontend/src/App.js
+++ Real/frontend/src/App.js
@@ -4,6 +4,7 @@ import Inputs from './pages/Inputs.js';
 function App() {
   return (
     <div className="App">
+      <title>Life Cycle Model</title>
       <header className="App-header">
         <Inputs></Inputs>
       </header>
